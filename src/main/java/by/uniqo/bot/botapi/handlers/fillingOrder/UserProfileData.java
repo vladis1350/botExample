355f94b67aa8bc68b02af02d0fd439eec94eaff9
andrey zapolskiy <uniqo@yandex.by>
2020-09-12 12:14:15 +0300

    override method toString in UserProfileData.java

--- src/main/java/by/uniqo/bot/botapi/handlers/fillingOrder/UserProfileData.java
+++ src/main/java/by/uniqo/bot/botapi/handlers/fillingOrder/UserProfileData.java
@@ -39,4 +39,19 @@ public class UserProfileData {
     int ribbonColor;
     int bowtieColor;
 
+    @Override
+    public String toString() {
+        return String.format("%s%nОбщая сумма лент %s%nЦвет лент %s%nНомер макета %s%nЦвет текста макета" +
+                        " %s%nНомер символа на ленте %s%nКол-во парней %s%nКол-во девушек %s%nКол-во преподавателей" +
+                        " %s%nНомер школы %s%nСтразы " +
+                        "%s%nМаленький колокольчик  %s%nБольшой колокольчик" +
+                        "  %s%nПригласительный свиток  %s%nБант  %s%nБабочка " +
+                        " %s%nФИО %s%nТелефонный номер %s%nСсылка в вк" +
+                        " %s%nАдрес доставки %s%nКомментарии к заказу %s",
+                "Данные по вашему заказу", getTotalNumber(), getTapesColor(), getModelNumber(),
+                getColorOfModelText(), getSymbolNumber(), getNumberOfMen(), getNumberOfWomen(),
+                getNumberOfTeacher(), getSchoolNumber(),getStars(),getLittleBell(),
+                getBigBell(), getScroll(), getRibbon(), getBowtie(), getCredentials(), getPhoneNumber(),
+                getUrlVK(), getDeliveryAddress(), getCommentsToOrder());
+    }
 }
